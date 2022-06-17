c087fcf999c44150a628adf02aecc73afae7ac93
Élisa Mabille <elisa.mabille@gmail.com>
2022-06-17 16:55:51 +0200

    First commit

---
+++ flaubert/js/script.js
@@ -0,0 +1,49 @@
+// Dark Mode
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        
+    	document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+// Iterate through divs
+
+$(document).ready(function(){
+
+	var params = new window.URLSearchParams(window.location.search); // Récupère tous les paramètres de l'URL
+	var myCurrentSection = params.get('id'); // Met dans une variable un paramètre en particulier
+	
+	// Affichage de la section courante
+	var myImageDiv = '#myImage'+ myCurrentSection; // Crée une chaîne de caractères
+	var myTextDiv = '#myText'+ myCurrentSection;
+	$(myImageDiv).show(); // JQuery appelle la section correspondante à partir de la nouvelle chaîne de caractères
+	$(myTextDiv).show();
+	
+	// Modifier l'URL du bouton previous
+	var myPreviousSection = parseInt(myCurrentSection) - 1;
+	var urlPrevious = "http://localhost:8888/Flaubert/g340.html?id=" + myPreviousSection;
+	$("#previous").attr("href", urlPrevious);
+		
+	// Modifier l'URL du bouton next
+	var myNextSection = parseInt(myCurrentSection) + 1;
+	var urlNext = "http://localhost:8888/Flaubert/g340.html?id=" + myNextSection;
+	$("#next").attr("href", urlNext);
+});
---
+++ flaubert/src/interface.py
@@ -0,0 +1,138 @@
+# Librairies #
+
+from bs4 import BeautifulSoup
+import os
+import re
+import shutil
+
+# Interface #
+
+def makeInterface(sourceXml, sourceHtml, targetHtml, tempFile):
+
+	# Transformation des éléments XML en HTML #
+	
+	with open(sourceXml, "r", encoding="utf-8") as file:
+	
+		soup = BeautifulSoup(file, "html.parser")
+	
+		myText = soup.find("text")
+		myText.name = "div"
+		myText.attrs = {"id" : "text", "class" : "row"}
+					
+		for elt in myText.find_all("add"):
+			elt.name = "ins"
+		
+		for elt in myText.find_all("hi", attrs = {"rend" : "u"}):
+			elt.name = "span"
+			elt.attrs = {"class" : "underline"}
+			
+		for elt in myText.find_all("head"):
+			elt.name = "h2"
+			
+		for elt in myText.find_all("lb"):	
+			elt.name = "br"
+			
+		for elt in myText.find_all("note", attrs = {"place" : "margin-right"}):
+			elt.name = "p"
+			elt.attrs = {"class" : "note"}
+			
+		for elt in myText.find_all("p"):
+			elt.name = "p"
+			elt.attrs = {"class" : "main"}
+			
+		for elt in myText.find_all("note", attrs = {"place" : "margin-left"}):
+			elt.name = "p"
+			elt.attrs = {"class" : "note"}
+	
+	with open(tempFile, "w", encoding="utf-8") as file:
+		file.write(str(myText))
+		
+	# REGEX #
+			
+	with open(tempFile, "r", encoding="utf-8") as file:
+	
+		linesRaw = file.readlines()
+		linesCorr = []
+		for line in linesRaw:
+			line = re.sub('<div n="[0-9]" type="(.*?)">', "", line)
+			line = re.sub("</div>", "", line)
+			line = re.sub('<pb facs="(.*?)" n="(.*?)">', r'</div><div class="col-6 myImage"><img src="media/\1"></div><div class="col-6 myText">', line)
+			line = re.sub("</pb>", "", line)
+			line = re.sub(r'^\s*$', "", line)
+			line = re.sub("<body>", "<div>", line)
+			line = re.sub("</body>", "</div></div>", line)
+			line = re.sub("<sp>", "", line)
+			line = re.sub("</sp>", "", line)
+			line = re.sub("<speaker>", "<br></br>", line)
+			line = re.sub("</speaker>", "", line)
+			linesCorr.append(line)
+			
+	with open(tempFile, "w", encoding="utf-8") as file:
+			
+		for line in linesCorr:
+			file.write(line)
+		print("Done", "Interface adjustment : ", sourceXml)
+	
+	# Création du fichier final #
+	
+	with open(tempFile, "r", encoding="utf-8") as file:
+		soup = BeautifulSoup(file, "html.parser")
+	
+		myText = soup.find("div", attrs = {"id" : "text"})
+						
+	with open(sourceHtml, "r", encoding="utf-8") as file:
+		soup = BeautifulSoup(file, "html.parser")
+		
+		div = soup.find("div", attrs = {"id" : "mainText"})
+		div.clear()
+		div.append(myText)
+		
+	with open(targetHtml, "wb") as fichier:
+		fichier.write(soup.prettify("utf-8"))
+		print("Done", "Create target HTML file : ", targetHtml)
+	
+	# Numérotation des class #
+	
+	with open(targetHtml, "r", encoding="utf-8") as file:
+		soup = BeautifulSoup(file, "html.parser")
+		myText = soup.find("div", attrs = {"id" : "text"})
+		
+		# Traitement des images #
+		i = 0
+		for elt in myText.find_all("div", {"class": "myImage"}):
+			myImageId = "myImage" + str(i)
+			elt.attrs = {"id" : myImageId, "class" : "col-12 col-lg-6 myImage card overflow-auto", "style" : "display:none; max-width:100%; max-height:80vh; height:auto;"}
+			i = i+1
+			
+		# Traitement des textes #
+		j = 0
+		for elt in myText.find_all("div", {"class": "myText"}):
+			myTextId = "myText" + str(j)
+			elt.attrs = {"id" : myTextId, "class" : "col-12 col-lg-6 myText card overflow-auto", "style" : "display:none; max-height:80vh"}
+			j = j+1
+	
+	with open(targetHtml, "r", encoding="utf-8") as file:
+		soup = BeautifulSoup(file, "html.parser")
+	
+		div = soup.find("div", attrs = {"id" : "mainText"})
+		div.clear()
+		div.append(myText)
+		
+	with open(targetHtml, "wb") as fichier:
+		fichier.write(soup.prettify("utf-8"))
+		print("Done", "Make interface", targetHtml)
+	
+def main():
+
+	sourceXml = os.path.abspath("../corpus/flaubert_g340.xml")
+	sourceHtml = os.path.abspath("../src/lorem-ipsum.html")
+	targetHtml = os.path.abspath("../g340.html")
+	tempFile = os.path.abspath("../src/tempFile.xml")
+	makeInterface(
+		sourceXml=sourceXml,
+		sourceHtml=sourceHtml,
+		targetHtml=targetHtml,
+		tempFile=tempFile,
+	)
+
+main()
